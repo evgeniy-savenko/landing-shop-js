f40b49562b146a4eba6e7280662cd98c616cef1f
Evgeniy Savenko <wayrez27@gmail.com>
2022-09-26 23:01:09 +0300

    add: accordion

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -21,4 +23,6 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-transparent', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    pictureSize('.sizes-block');
+    accordion('.accordion-heading');
 });
